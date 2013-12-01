bc6dce8a34e581943d9ac850a7625f140bdfa19b
Gustavo Carvalho <gdacarv@gmail.com>
2013-12-01 08:59:55 -0300

    Before merge.

--- src/com/hackathon/comparadorloa/CompararServlet.java
+++ src/com/hackathon/comparadorloa/CompararServlet.java
@@ -19,14 +19,16 @@ import com.google.appengine.labs.repackaged.org.json.JSONObject;
 
 public class CompararServlet extends HttpServlet {
 
-	private static final String COMPARAR_URL = "http://orcamento.dados.gov.br/sparql/?default-graph-uri=&query=SELECT+%%3FfuncaoCodigo+%%3FsubfuncaoCodigo+%%28sum%%28%%3Fval%%29+as+%%3Fpago%%29+WHERE+%%7B%%0D%%0A%%3Fi+loa%%3AtemFuncao+%%3Ffuncao+.+%%0D%%0A%%3Ffuncao+rdf%%3Alabel+%%3FfuncaoNome+.++%%0D%%0A%%3Ffuncao+loa%%3Acodigo+%%3FfuncaoCodigo+.%%0D%%0A%%3Fi+loa%%3AtemSubfuncao+%%3Fsubfuncao+.++%%0D%%0A%%3Fsubfuncao+rdf%%3Alabel+%%3FsubfuncaoNome+.+%%0D%%0A%%3Fsubfuncao+loa%%3Acodigo+%%3FsubfuncaoCodigo+.%%0D%%0A%%3Fi+loa%%3AvalorPago+%%3Fval+.%%0D%%0A%%3Fi+loa%%3AtemExercicio+%%3Fexercicio+.%%0D%%0A%%3Fexercicio+loa%%3Aidentificador+%%3Fano+.%%0D%%0AFILTER+%%28%%28%%28%%3FfuncaoCodigo+%%3D+%%22%s%%22+%%26%%26+%%3FsubfuncaoCodigo+%%3D+%%22%s%%22%%29+%%7C%%7C+%%28%%3FfuncaoCodigo+%%3D+%%22%s%%22+%%26%%26+%%3FsubfuncaoCodigo+%%3D+%%22%s%%22%%29%%29+%%26%%26+%%3Fano+%%3E%%3D+%s+%%26%%26+%%3Fano+%%3C%%3D+%s%%29+.%%0D%%0A%%7D&debug=on&timeout=&format=json&save=display&fname=";
-	
+	private static final String COMPARAR_URL = "http://orcamento.dados.gov.br/sparql/?default-graph-uri=&query=SELECT+%%3FfuncaoCodigo+%%28sum%%28%%3Fval%%29+as+%%3Fpago%%29+WHERE+%%7B%%0D%%0A%%3Fi+loa%%3AtemFuncao+%%3Ffuncao+.+%%0D%%0A%%3Ffuncao+rdf%%3Alabel+%%3FfuncaoNome+.++%%0D%%0A%%3Ffuncao+loa%%3Acodigo+%%3FfuncaoCodigo+.%%0D%%0A%%3Fi+loa%%3AtemSubfuncao+%%3Fsubfuncao+.++%%0D%%0A%%3Fsubfuncao+rdf%%3Alabel+%%3FsubfuncaoNome+.+%%0D%%0A%%3Fsubfuncao+loa%%3Acodigo+%%3FsubfuncaoCodigo+.%%0D%%0A%%3Fi+loa%%3AvalorPago+%%3Fval+.%%0D%%0A%%3Fi+loa%%3AtemExercicio+%%3Fexercicio+.%%0D%%0A%%3Fexercicio+loa%%3Aidentificador+%%3Fano+.%%0D%%0AFILTER+%%28%%28%%28%%3FfuncaoCodigo+%%3D+%%22%s%%22%s%%29+%%7C%%7C+%%28%%3FfuncaoCodigo+%%3D+%%22%s%%22%s%%29%%29+%%26%%26+%%3Fano+%%3E%%3D+%s+%%26%%26+%%3Fano+%%3C%%3D+%s%%29+.%%0D%%0A%%7D&debug=on&timeout=&format=json&save=display&fname=";
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		int index;
 		String funcao1 = req.getParameter("Funcao1");
-		URL url = new URL(String.format(COMPARAR_URL, funcao1, req.getParameter("Subfuncao1"), req.getParameter("Funcao2"), req.getParameter("Subfuncao2"), req.getParameter("inicio"), req.getParameter("fim")));
+		String subfuncao1 = req.getParameter("Subfuncao1");
+		String subfuncao2 = req.getParameter("Subfuncao2");
+		URL url = new URL(String.format(COMPARAR_URL, funcao1, subfuncao1.equals("0") || subfuncao1.isEmpty() ? "" : "+%26%26+%3FsubfuncaoCodigo+%3D+%22"+subfuncao1+"%22", req.getParameter("Funcao2"), subfuncao2.equals("0") || subfuncao2.isEmpty()  ? "" : "+%26%26+%3FsubfuncaoCodigo+%3D+%22"+subfuncao2+"%22", req.getParameter("inicio"), req.getParameter("fim")));
 		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
 		conn.setRequestProperty("Accept-Charset", "UTF-8");
 		conn.setConnectTimeout(60000);
